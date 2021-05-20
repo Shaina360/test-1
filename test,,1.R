ed1f6f327c26ce01eaae47b0d922478c77e953c0
shaina360 <shainaverma7222@gmail.com>
2021-05-20 15:07:22 +0530

    first commit

---
+++ test,,1.R
@@ -0,0 +1,43 @@
+#1. what is r and r studio
+
+r is  progogram language that act as IDE(integrated development program)
+
+#2. store any value in any variable ...elt(mixbag
+mixbag <- c(6,12,17)
+
+#3. what is vector
+homologus signle dimensional data structure, that can be change
+
+#4. store value in vector as character,logical and numerical
+vec1 <- c(1,2,3)
+vec2 <- c("A")
+vec3<-c(1,"A",T)
+#extract the value from mixbag as store above
+mixbag[3]
+
+#5. extrace the character value 
+vec2
+
+#6. LIST ;
+heterogeneous data structure elememt identify remains same
+L1 <-list(1,"A",T)
+L1
+
+#7. MATRIX ; 
+Homogeneous multidimentional data structure
+b <- matrix(c(3:10), nrow = 4, byrow = T)
+rownames = c("row1","row2","row3","row4")
+colnames = c("col1","col2")
+result <- matrix(c(3:10), nrow = 4, byrow = T, dimnames = list(rownames,colnames))
+result            
+
+#8. how to create repository?
+git login > profile+signin  > new repsotiry > name the repository > crete > copy link > r.studio > file>
+  >newproject > version control > git > paste > clone the project
+
+#9.switch directory
+session>setworkingdirectory>choose directory > choose directory 
+
+#10. git
+version control systen,open source
+
